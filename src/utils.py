7df6155508bf90a6516034be5dbf83358bf9817d
Olivier Chapeau <ochapeau@protonmail.com>
2024-03-05 14:08:21 +0100

    Updated utils docstrings

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,13 @@ import yaml
 
 # YAML
 def load_yaml(file_path: str) -> dict[str, any]:
-    """Loads the content of a yaml file in a string or a dict
+    """Loads the content of a yaml file in a string or a dict.
 
     Args:
-        file_path (str): Name of the yaml file to load
+        file_path (str): Name of the yaml file to load.
 
     Returns:
-        dict[str, any]: Content of the yaml file
+        dict[str, any]: Content of the yaml file.
     """
     with open(file=file_path, mode="r") as file:
         data = yaml.load(file, Loader=yaml.SafeLoader)
@@ -19,18 +19,28 @@ def load_yaml(file_path: str) -> dict[str, any]:
 
 
 def save_yaml(file_path: str, data: dict[str, any]) -> None:
-    """_summary_
+    """Saves the content of a dict to a yaml file.
 
     Args:
-        file_path (str): _description_
-        data (Dict[str, any]): _description_
+        file_path (str): Name of the yaml file to write.
+        data (Dict[str, any]): Dict with the data to write.
     """
     with open(file=file_path, mode="w") as file:
         yaml.dump(data, file, default_flow_style=False)
 
 
 # Languages
-def load_languages(config):
+def load_languages_files(config: dict[str, any]):
+    """Load the languages strings into streamlit session_state.
+    Looks for the keys 'languages' and 'default_language',
+    like in the following yaml sample:
+        languages:
+            [language_code]: [strings_yaml_file]
+        default_language: [language_code]
+
+    Args:
+        config (dict[str, any]): Configuration file loaded in a dictionary.
+    """
     # Session State keys
     languages_key = "languages"
     strings_key = "strings"
@@ -40,14 +50,15 @@ def load_languages(config):
     # Loading the languages files into the session_state object
     for language, file in config[languages_key].items():
         st.session_state[languages_key][language] = load_yaml(file)
-    # Inirializing the strings
+    # Initializing the strings
     if strings_key not in st.session_state:
         st.session_state[strings_key] = st.session_state[languages_key][
             config["default_language"]
         ]
 
 
-def update_language():
+def update_language() -> None:
+    """Updates the 'st.session_state.strings' to the strings of the 'st.session_state.language'"""
     selected_language = st.session_state.language
     st.session_state.strings = st.session_state.languages[selected_language]
 
@@ -69,25 +80,41 @@ def check_required_session_state_keys(required_keys: list[str]) -> bool:
     """Check if specified keys exist and are not None in Streamlit's session state.
 
     Args:
-    required_keys (List[str]): A list of keys to check in the session state.
+        required_keys (List[str]): A list of keys to check in the session state.
 
     Returns:
-    bool: True if all specified keys exist and their values are not None, False otherwise.
+        bool: True if all specified keys exist and their values are not None, False otherwise.
     """
     return all(st.session_state.get(key) is not None for key in required_keys)
 
 
 # Streamlit-Authenticator
 def get_email_for_username(file_path: str, username: str) -> str:
+    """Returns the email address for a given username.
+
+    Args:
+        file_path (str): File path for the streamlit-authenticator configuration file.
+        username (str): Username of a user present in streamlit-authenticator configuration file.
+
+    Returns:
+        str: Email address for the given username.
+    """
     data = load_yaml(file_path=file_path)
     return data["credentials"]["usernames"][username]["email"]
 
 
 def get_authenticator(config: dict) -> stauth.Authenticate:
-    authenticator = stauth.Authenticate(
+    """Returns a streamlit-authenticator Authenticate object from a streamlit-authenticator dictionary.
+
+    Args:
+        config (dict): streamlit-authenticator dictionary.
+
+    Returns:
+        stauth.Authenticate: The streamlit-authenticator Authenticate object.
+    """
+    return stauth.Authenticate(
         config["credentials"],
         config["cookie"]["name"],
         config["cookie"]["key"],
         config["cookie"]["expiry_days"],
     )
-    return authenticator
